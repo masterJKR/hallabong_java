e9752ebb5607bb521c2796f324d0ed04fe4c03f6
masterJKR <nilzero25@gamil.com>
2026-01-05 10:25:14 +0900

    1월 5일 if문 주차요금

--- java_control/src/study20260102/MainMethod.java
+++ java_control/src/study20260102/MainMethod.java
@@ -1,5 +1,7 @@
 package study20260102;
 
+import java.util.Scanner;
+
 public class MainMethod {
 
 	public static void main(String[] args) {
@@ -31,11 +33,34 @@ public class MainMethod {
 		
 		//  주차장에  요금이  기본요금은 1000원이다.
 		//  기본시간은 30분이다.
-		// 10분 당 100원씩 추가  예)  39분은 1000원,  40분은 1100원
+		//  10분 당 100원씩 추가  예)  39분은 1000원,  40분은 1100원
 		//  단  4시간 이상 주차시  기본요금은 2500원이다.  
 		//   예)  3시간 50분 (230분) -> 3000원 
-		///       4시간 12분 ( 252분) -> 2600원
+		//       4시간 12분 ( 252분) -> 2600원
 		
+		int cost = 1000;// 기본요금 (30분)
+		int inc = 100, incTime = 10; // 10분당 100원
+		int time = 30; // 기본 시간 30분
+		
+		//  주차 시간 총 몇분  입력 
+		Scanner scan = new Scanner(System.in); // 시스템(컴퓨터)으로부터 입력된키 값 받기
+		
+		System.out.print(" 총 몇분 주차 : ");
+		int pTime = scan.nextInt(); // 키보드 입력 값 받아서 정수로 변환하여 변수에 저장
+		
+		pTime /= 10;  //  pTime = pTime/10
+		if( pTime >= 24) { // 4시간 이상 주차했다면   pTime >= 240
+			cost=2500; // 4시간 이상에 대한 기본요금 변경
+			pTime -= 24; // 4시간 빼기 , 4시간에 대한 요금은 2500원 나머지는 10분당요금
+		}else if( pTime > 3 ) { 
+			// 4시간 이상이 아니라면,  기본시간 30분으로 계산해야 된다.
+			pTime -= 3; // 기본시간이 30분이니까 3빼기
+		}else { // 최초 기본시간 안에서 주차 한 경우
+			pTime=0;
+		}
+		// 주차 요금 계산
+		int price = cost + pTime*100;
+		System.out.println(" 주차요금 : "+ price +"원");
 		
 	}
 
