a40d7d87ce0a5aa2c98100c510e2c608449a5e98
masterJKR <nilzero25@gamil.com>
2026-01-16 11:22:04 +0900

    1월 16일 인터페이스

---
+++ java_inherit/src/dto/CardAttend.java
@@ -0,0 +1,26 @@
+package dto;
+
+import able.Attendance;
+
+public class CardAttend implements Attendance {
+
+	private boolean present = false; // 처음에는 퇴실상태
+	
+	@Override
+	public void checkIn() {
+		present = true; // 출석 했다.
+		System.out.println("카드 출석 완료");
+	}
+
+	@Override
+	public void checkOut() {
+		present = false; //퇴실했다.
+		System.out.println("카드 퇴실 완료");
+	}
+
+	@Override
+	public boolean isPresent() { // 현재 상태 
+		return present;
+	}
+
+}
---
+++ java_inherit/src/mainpkg/MainMethod.java
@@ -0,0 +1,17 @@
+package mainpkg;
+
+import able.Actionable;
+import dto.Member;
+
+public class MainMethod {
+
+	public static void main(String[] args) {
+//		Actionable member = new Member("skyblue","이순신",34);
+//		
+//		member.move();
+//		member.drink();
+//		System.out.println( member.toString());
+		
+	}
+
+}
